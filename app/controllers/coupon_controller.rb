a4352b00189a1cafad03294d2d7679dadc69dd48
Melissa Real <melissa.real@hotmail.com>
2019-01-02 14:33:26 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupon_controller.rb
@@ -0,0 +1,21 @@
+class CouponsController < ApplicationController
+  def index
+    @controllers = Controller.all
+  end
+
+  def show
+    @controller = Controller.find(params[:id])
+  end
+
+  def new
+    @controller = Controller.new
+  end
+
+  def create
+    @controller = Controller.new
+    @controller.coupon_code = params[:coupon_code]
+    @controller.store = params[:store]
+    @controller.save
+    redirect_to controller_path(@controller)
+  end
+end
